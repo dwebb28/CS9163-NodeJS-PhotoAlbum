f20c6dacdfe05ef9570d3e085b44aa61b68ad041
Dan <rudwebb2014@gmail.com>
2017-10-03 22:02:33 -0400

    updating errors and views

--- webapp/app.js
+++ webapp/app.js
@@ -12,27 +12,29 @@ var mysql = require('mysql');
 var cookie = require('cookie');
 console.log(__dirname);
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//establish the database connection details
 var conn = mysql.createConnection({
 	host: "localhost",
 	user: "sql_admin",
 	password: "appsec"
 });
 
+//connect to the database
 conn.connect(function(err){
 	if (err) throw err;
 	console.log("Connected to mysql database");
 });
 
-app.use(fileUpload());
-app.use(bodyParser.urlencoded({ extended: true }));
-
+//have app listening on port 3000
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000!');
 })
 
+//generic function to query the database by passing in sql string
 function queryDatabase(sql, callback){
 	
 	conn.query(sql, function(error, results,fields){
@@ -42,6 +44,8 @@ function queryDatabase(sql, callback){
 	});
 }
 
+//function used to compare the hash values of two password hashes
+//used on the login page
 function compareHash(password, hash, callback){
 	
 	bcrypt.compare(password, hash, function(err, res, redirect_user) {
@@ -56,98 +60,142 @@ function compareHash(password, hash, callback){
 				});
 }
 
+//used to parse the cookie
 function parseCookie(req, callback){
 	console.log(req.headers.cookie);
 	
 	if(req.headers.cookie === undefined)
 	{
 		callback(undefined);
+	} else{
+		var cook = cookie.parse(req.headers.cookie);
+		console.log(cook);
+		callback(cook);
 	}
-	
-	var cook = cookie.parse(req.headers.cookie);
-	console.log(cook);
-	callback(cook);
 }
 
+//used to physically upload the pictures onto the server
+//does some error checking
+//saves the picture's path and comments into the database
 app.post('/upload', function(req, res){
 	
 	var username;
 	parseCookie(req, function(cook){
 		if(!cook)
+		{
 			res.redirect('/login');
-		
-		username = cook.username;
+		}
+		else{
+			username = cook.username;
+			
+			//check to make sure files were uploaded
+			if(!req.files || Object.keys(req.files) == 0)
+			{
+				return res.status(400).send('No files were uploaded');
+			}
+			else{
+				let fileToUpload = req.files.picture;
+				var picName = fileToUpload.name;
+				
+				//make sure that the file being uploaded is actuallly a valid file type supported
+				if( !(picName.toLowerCase().endsWith('.jpg')) && !(picName.toLowerCase().endsWith('.png')) && !(picName.toLowerCase().endsWith('.gif') ))
+				{
+					res.status(400).send('Invalid file type');
+				}
+				else{
+					var filepath = 'images/' + picName;
+				
+					var comment = req.body.comment;
+					
+					//save the picture to the server
+					fileToUpload.mv( 'public/' + filepath, function(err){
+						if(err){
+							return res.status(500).send(err);
+						}
+						else
+						{
+							var sql = 'INSERT INTO photoapp.photos(username,file_path,comment,insert_date_time,update_date_time) values('
+									+ conn.escape(username) + ',' + conn.escape(filepath) + ',' + conn.escape(comment) + ',' + conn.escape(new Date()) + ',' + conn.escape(new Date()) + ')';
+						
+							console.log(sql);
+							
+							//save the picture information to the database for future retrieval
+							queryDatabase(sql, function(insert_results){
+											if(insert_results){
+												res.redirect('/home');
+											}
+										});
+						}
+					});
+					
+					
+				}
+				
+			}
+		}
 	});
-
-	if(!req.files)
-		return res.status(400).send('No files were uploaded');
-	
-	let fileToUpload = req.files.picture;
-	var picName = fileToUpload.name;
-	var filepath = 'images/' + picName;
-	
-	var comment = req.body.comment;
-	
-	fileToUpload.mv( 'public/' + filepath, function(err){
-		if(err)
-			return res.status(500).send(err);
-	});
-	
-	var sql = 'INSERT INTO photoapp.photos(username,file_path,comment,insert_date_time,update_date_time) values('
-		+ conn.escape(username) + ',' + conn.escape(filepath) + ',' + conn.escape(comment) + ',' + conn.escape(new Date()) + ',' + conn.escape(new Date()) + ')';
-		
-	console.log(sql);
-	
-	queryDatabase(sql, function(insert_results){
-					if(insert_results){
-						res.redirect(req.headers.origin + '/home');
-					}
-				});
-	
 });
 
+//get the upload page
 app.get('/upload', function(req, res){
-	res.sendFile(path.join(__dirname, '/web/' , 'upload.html'));
+	//parse cookie
+	parseCookie(req, function(cook){
+		if(!cook)
+			//if cookie not valid
+			res.redirect('/login');
+		else
+			res.sendFile(path.join(__dirname, '/web/' , 'upload.html'));
+	});
+	
 });
 
+//get the home page for that user logged in
 app.get('/home', function(req, res){
 	
 	var username;
 	parseCookie(req, function(cook){
 		
 		if(!cook){
+			//if cookie is invalid
 			res.redirect('/login');
 		}
+		else{
+			username = cook.username;
+			var start = parseInt(req.params.start, 10);
+			var limit = 200;
+			var pictures = [];
+			console.log('Getting pictures');
+			
+			//get all pictures for this user
+			getPicturesByUserName( username ,function(results){
+				var lastId = start;
+				
+				//for all of the results of the query, create objects
+				//with the filepath and the comment, that will be used
+				//in the ejs file to insert them as images into the page
+				for(var i = 0 ; i < results.length ; i++){
+					var picture = results[i];
+					
+					if(picture.comment === null)
+						picture.comment = '';
+					
+					pictures.push({
+						file_path : '/' + picture.file_path,
+						comment: picture.comment
+					})
+				}
+				
+				//render the page and insert the list of pictures
+				res.render('pages/home',{
+				pictures: pictures
+			});
 		
-		username = cook.username;
-	});
-	
-	var start = parseInt(req.params.start, 10);
-	var limit = 200;
-	var pictures = [];
-	console.log('Getting pictures');
-	getPicturesByUserName( username ,function(results){
-		var lastId = start;
-		for(var i = 0 ; i < results.length ; i++){
-			var picture = results[i];
-			
-			if(picture.comment === null)
-				picture.comment = '';
-			
-			pictures.push({
-				file_path : '/' + picture.file_path,
-				comment: picture.comment
-			})
-			lastId = picture.id;
+			});
 		}
-		console.log(pictures);
-		res.render('pages/home',{
-		pictures: pictures
-	});
-		
-	});
+	});	
 });
 
+//Once logged in a user can create edit their own pictures
 app.get('/home/editPictures', function(req, res){
 	
 	var username;
@@ -155,85 +203,60 @@ app.get('/home/editPictures', function(req, res){
 		
 		if(!cook){
 			res.redirect('/login');
+		} else
+		{
+			//get the username from the cookie
+			username = cook.username;
+			var start = parseInt(req.params.start, 10);
+			var limit = 200;
+			var pictures = [];
+			console.log('Getting pictures');
+			
+			//for all of the results of the query, create objects
+			//with the filepath and the comment, that will be used
+			//in the ejs file to insert them as images into the page
+			getPicturesByUserName( username ,function(results){
+				var lastId = start;
+				for(var i = 0 ; i < results.length ; i++){
+					var picture = results[i];
+					
+					if(picture.comment === null)
+						picture.comment = '';
+					
+					//create objects with the file path to be rendered
+					//provide the comment to be put into an editable textbox for updates
+					//provide the id, for deletion to call the delete function
+					pictures.push({
+						file_path : '/' + picture.file_path,
+						comment: picture.comment,
+						id: picture.id
+					})
+				}
+				console.log(pictures);
+				res.render('pages/edit',{
+				pictures: pictures
+			});				
+			});
 		}
 		
-		username = cook.username;
+		
 	});
 	
-	var start = parseInt(req.params.start, 10);
-	var limit = 200;
-	var pictures = [];
-	console.log('Getting pictures');
-	getPicturesByUserName( username ,function(results){
-		var lastId = start;
-		for(var i = 0 ; i < results.length ; i++){
-			var picture = results[i];
-			
-			if(picture.comment === null)
-				picture.comment = '';
-			
-			pictures.push({
-				file_path : '/' + picture.file_path,
-				comment: picture.comment,
-				id: picture.id
-			})
-			lastId = picture.id;
-		}
-		console.log(pictures);
-		res.render('pages/edit',{
-		pictures: pictures
-	});
-		
-	});
+	
 });
 
-app.get('/home/editPictures', function(req, res){
-	
-	var username;
-	parseCookie(req, function(cook){
-		
-		if(!cook){
-			res.redirect('/login');
-		}
-		
-		username = cook.username;
-	});
-	
-	var start = parseInt(req.params.start, 10);
-	var limit = 200;
-	var pictures = [];
-	console.log('Getting pictures');
-	getPicturesByUserName( username ,function(results){
-		var lastId = start;
-		for(var i = 0 ; i < results.length ; i++){
-			var picture = results[i];
-			
-			if(picture.comment === null)
-				picture.comment = '';
-			
-			pictures.push({
-				file_path : '/' + picture.file_path,
-				comment: picture.comment
-			})
-			lastId = picture.id;
-		}
-		console.log(pictures);
-		res.render('pages/home',{
-		pictures: pictures
-	});
-		
-	});
-});
-
+//no authentication needed, provide the login page
 app.get('/login', function(req,res){
 	res.sendFile(path.join(__dirname, '/web', 'login.html'));
 });
 
+//log the user out- clear their cookie, and redirect back to the login page
 app.get('/logout', function(req,res){
 	res.clearCookie('username');
 	res.redirect('/login');
 });
 
+//log the user in
 app.post('/login', function(req,res){
 	console.log(req.body);
 	
@@ -241,8 +264,6 @@ app.post('/login', function(req,res){
 		var password = req.body.psw;
 		var sql = 'SELECT password_hash FROM photoapp.users WHERE username = ' + conn.escape(username);
 		console.log(sql);
-		var auth_success = false;
-		
 
 		queryDatabase(sql, function(results){
 			
@@ -250,8 +271,12 @@ app.post('/login', function(req,res){
 				console.log('User exists...checking hash');
 				var hash = results[0].password_hash;
 				
+				//pass the hash from the database into this function
+				//to compare against the generated hash from the plaintext password
 				compareHash(password, hash, function(success){
 					
+					//if the password hashes match, set the cookie
+					//redirect to the home page
 					if(success){
 						res.setHeader('Set-Cookie', cookie.serialize('username', username, {
 							httpOnly: true,
@@ -265,7 +290,7 @@ app.post('/login', function(req,res){
 					}
 				});
 				
-				
+			//if the logins do not match, redirect back to login page
 			} else{
 				res.redirect('/login');
 			}
@@ -273,10 +298,13 @@ app.post('/login', function(req,res){
 	
 });
 
+//provide the registration page
 app.get('/register', function(req,res){
 	res.sendFile(path.join(__dirname, '/web/' , 'register.html'));
 });
 
+//registration function, creates the password hash, saves to the db and then sets the cookie
+//on the users browser
 app.post('/register',function(req,res){
 	
 	var username = req.body.uname;
@@ -298,6 +326,7 @@ app.post('/register',function(req,res){
 				console.log(insert);
 				
 				queryDatabase(insert, function(insert_results){
+					//if successfully saved, set the user's cookie on their browser
 					if(insert_results){
 						res.setHeader('Set-Cookie', cookie.serialize('username', username, {
 							user: username,
@@ -314,7 +343,7 @@ app.post('/register',function(req,res){
 	});
 });
 
-
+//delete picture by the picture id
 function deletePicture( pictureId, username, callback){
 	var sql = 'DELETE FROM photoapp.photos WHERE id = ' + conn.escape(pictureId) + ' and username = ' + conn.escape(username);
 	console.log('sql: ' + sql);
@@ -324,8 +353,9 @@ function deletePicture( pictureId, username, callback){
 	});
 }
 
+//edit the comment of the picture
 function editPicture( pictureId, comment, username, callback){
-	var sql = 'UPDATE photoapp.photos SET comment = ' + conn.escape(comment) + ' WHERE username = ' + conn.escape(username) + ' AND id = ' + conn.escape(pictureId);
+	var sql = 'UPDATE photoapp.photos SET comment = ' + conn.escape(comment) + ', update_date_time = ' + conn.escape(new Date()) + ' WHERE username = ' + conn.escape(username) + ' AND id = ' + conn.escape(pictureId);
 	console.log('sql: ' + sql);
 	queryDatabase(sql, function(results){
 		console.log(results);
@@ -333,6 +363,8 @@ function editPicture( pictureId, comment, username, callback){
 	});
 }
 
+//get pictures starting at an index, and for a certain amount
+//used to only return the first 10, second 10, etc. pictures from the database
 function getPictures( start , limit, callback ){
 	var sql = 'SELECT * FROM photoapp.photos ORDER BY update_date_time DESC LIMIT ' + conn.escape(start) + ' , ' + conn.escape(limit);
 	console.log('sql: ' + sql);
@@ -342,20 +374,26 @@ function getPictures( start , limit, callback ){
 	});
 }
 
+//Get all pictures by the username, used in the home and edit picture pages
 function getPicturesByUserName( username, callback ){
-	var sql = 'SELECT * FROM photoapp.photos WHERE username = ' + conn.escape(username);
+	var sql = 'SELECT * FROM photoapp.photos WHERE username = ' + conn.escape(username) + ' ORDER BY update_date_time DESC';
+	console.log('Get Pictures By username: ' + sql);
 	queryDatabase(sql, function(results){
 		console.log(results);
 		callback(results);
 	});
 }
 
+//default redirect to start at the top of the pictures page
 app.get('/pictures', function(req, res){
 	res.redirect('/pictures/0');
 });
 
+//gets all pictures in chronological order starting from position
+//:start in the list
 app.get('/pictures/:start', function(req, res){
 	var start = parseInt(req.params.start, 10);
+	//only want to return 10 at a time at most
 	var limit = 10;
 	var pictures = [];
 	console.log('Getting pictures');
@@ -372,12 +410,14 @@ app.get('/pictures/:start', function(req, res){
 			})
 		}
 		console.log(pictures);
+		//do the logic to determine which picture is the last one
+		//helps to create the hyperlink for the next and back pages
 		var lastPic = (start + pictures.length);
 		var previous = 0;
 		if(start >= 10){
 			previous = start - 10;
 		}
-		
+		//render the page and use the insert variables to setup the page
 		res.render('pages/pictures',{
 		pictures: pictures,
 		lastPicture : lastPic,
@@ -390,25 +430,28 @@ app.get('/pictures/:start', function(req, res){
 	
 });
 
+//delete picture by id
 app.get('/deletePicture/:pictureId', function(req, res){
 	
 	var pictureId = parseInt(req.params.pictureId);
 	var username;
 	parseCookie(req, function(cook){
-		
+		//authenticate user
 		if(!cook){
 			res.redirect('/login');
-		}		
-		username = cook.username;
-	});
-
-	deletePicture( pictureId, username ,function(results){
-		if(results){
-			res.redirect('/home');
+		}
+		else{
+			username = cook.username;
+			deletePicture( pictureId, username ,function(results){
+				if(results){
+					res.redirect('/home');
+				}
+			});
 		}
 	});
 });
 
+//edit picture by the picture id
 app.post('/editPicture/:pictureId', function(req, res){
 	
 	var pictureId = parseInt(req.params.pictureId);
@@ -416,15 +459,28 @@ app.post('/editPicture/:pictureId', function(req, res){
 	var username;
 	parseCookie(req, function(cook){
 		
+		//authenticate user
 		if(!cook){
 			res.redirect('/login');
 		}		
-		username = cook.username;
+		else
+		{
+			username = cook.username
+			editPicture( pictureId, comment , username ,function(results){
+			if(results){
+				res.redirect('/home');
+			}
 	});
-
-	editPicture( pictureId, comment , username ,function(results){
-		if(results){
-			res.redirect('/home');
 		}
 	});
+});
+
+//default redirect
+app.get('/*', function(req, res) {
+    res.redirect('/home')
+});
+
+//default redirect
+app.post('/*', function(req, res) {
+    res.redirect('/home')
 });
